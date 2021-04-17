ad3b2c62206fd72767efebad1011f2ac48285ea4
Ramesh Murugesan <me@macroramesh.com>
2021-04-17 07:01:11 +0100

    create user API

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,11 +1,22 @@
+const bcrypt = require('bcrypt');
 const User = require('../models').User;
 
 module.exports = {
   create(req, res) {
+
+
+    // audit
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).send()
+    }
+
+    // password hash
+    const passwordHash = bcrypt.hashSync(req.body.password, 8);
+
     return User
       .create({
         email: req.body.email,
-        password: req.body.password,
+        password: passwordHash,
       })
       .then(User => res.status(201).send(User))
       .catch(error => res.status(400).send(error));
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,13 @@
 const todosController = require('../controllers').todos;
 const todoItemsController = require('../controllers').todoItems;
+const userController = require('../controllers').users;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
+
+  app.post('/api/user', userController.create);
 
   app.post('/api/todos', todosController.create);
   app.get('/api/todos', todosController.list);
